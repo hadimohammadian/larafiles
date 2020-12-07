ad853c929b06f038ec4bdc80079845ff468c9b71
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-12-07 10:38:22 +0330

    add master page to project

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,31 @@
+<!
+DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/bootstrap-rtl.min.css">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+
+    @include('admin.partials.nav')
+
+
+      <div class="container">
+
+        @yield('content')
+
+      </div>
+
+
+
+      <script src="/js/jquery-3.5.1.slim.min.js"></script>
+      <script src="/js/bootstrap.min.js"></script>
+      <script src="/js/bootstrap.bundle.min.js"></script>
+
+
+</body>
+</html>
