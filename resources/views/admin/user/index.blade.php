bcd79288b8966f76e490edd08002510ff9ff4b77
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-12-07 14:28:11 +0330

    usercontroller index page

---
+++ resources/views/admin/user/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.admin')
+
+
+@section('content')
+
+<div class="m-3 text-right">
+    <div class="card">
+        <div class="card-header">
+            لیست کاربران
+        </div>
+        <div class="card-body">
+            <h4 class="card-title">پنل کاربران</h4>
+            <p class="card-text">لیست کاربران</p>
+        </div>
+
+    </div>
+
+</div>
+@endsection
