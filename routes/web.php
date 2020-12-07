c7809134901b0d331a768b8f7315fbae881ced02
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-12-07 10:39:28 +0330

    route to view dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\Routing\Route as RoutingRoute;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/admin', function () {
+
+    return view('admin.dashboard.index');
 });
