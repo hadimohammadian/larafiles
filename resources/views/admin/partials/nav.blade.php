699807d2161c854bfbc9bd03dad547f0fb7167ea
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-12-09 00:55:05 +0330

    add menu link with  route()

--- resources/views/admin/partials/nav.blade.php
+++ resources/views/admin/partials/nav.blade.php
@@ -1,41 +1,43 @@
 <div class="container">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">مدیریت فروشگاه فایل</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">مدیریت فروشگاه فایل</a>
+    {{-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
-        </button>
+        </button> --}}
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
 
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                کاربران
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">کاربر جدید</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">لیست کاربران</a>
-
-
-              </div>
-            </li>
-
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  فایل ها
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a class="dropdown-item" href="#">ثبت فایل جدید</a>
-                  <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="#">لیست فایل ها</a>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            کاربران
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="{{route('admin.users.create')}}">کاربر جدید</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="{{route('admin.users.list')}}">لیست کاربران</a>
 
 
-                </div>
-              </li>
+          </div>
+        </li>
 
-          </ul>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            فایل ها
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">ثبت فایل جدید</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">لیست فایل ها</a>
 
-        </div>
-      </nav>
+
+          </div>
+        </li>
+
+      </ul>
+
+    </div>
+  </nav>
 </div>
