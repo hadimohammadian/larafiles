ef88ab2b649d342955c40da8841a01e967921a58
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-12-09 00:55:25 +0330

    create view page

---
+++ resources/views/admin/user/create.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.admin')
+
+
+
+
+@section('content')
+
+<div class="m-3 text-right">
+    <div class="card">
+        <div class="card-header">
+            ثبت کاربران
+        </div>
+        <div class="card-body">
+            <h4 class="card-title">پنل کاربران</h4>
+            <p class="card-text">ثبت کاربران</p>
+        </div>
+
+    </div>
+
+</div>
+
+@endsection
