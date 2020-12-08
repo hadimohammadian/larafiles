4817588cd72fb54302a022624f32471260aab5d2
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-12-09 01:38:50 +0330

    update user list page

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -3,5 +3,33 @@
 
 @section('content')
 
- <h1>لیست کاربران</h1>
+<table class="table table-hover">
+    <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">نام</th>
+          <th scope="col">نام خانوادگی</th>
+          <th scope="col">متن</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td colspan="2">Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+  </table>
 @endsection
