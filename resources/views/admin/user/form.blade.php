6b6e665fe5f4a0e6a777d2852c6129fd0f3511e9
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-12-09 02:10:36 +0330

    update register form

--- resources/views/admin/user/form.blade.php
+++ resources/views/admin/user/form.blade.php
@@ -1,8 +1,48 @@
 <form action="" method="POST">
 
+@csrf
     <div class="input-group mb-3">
         <span class="input-group-text" id="inputGroup-sizing-default">نام کامل</span>
-        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" id="FullName" name="FullName">
-      </div>
+        <input type="text" class="form-control" aria-label="Sizing example input"
+            aria-describedby="inputGroup-sizing-default" id="FullName" name="FullName">
+    </div>
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-default">ایمیل</span>
+        <input type="email" class="form-control" aria-label="Sizing example input"
+            aria-describedby="inputGroup-sizing-default" id="email" name="email">
+    </div>
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-default">کلمه عبور</span>
+        <input type="password" class="form-control" aria-label="Sizing example input"
+            aria-describedby="inputGroup-sizing-default" id="password" name="password">
+    </div>
+
+
+    <div class="input-group mb-3">
+        <label class="input-group-text" for="inputGroupSelect02">نقش کاربری</label>
+        <select class="form-control " id="role" name="role">
+            <option selected>انتخاب ...</option>
+            <option value="1">مدیر</option>
+            <option value="2">اپراتور</option>
+            <option value="3">کاربر عادی</option>
+        </select>
+
+    </div>
+
+
+
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-default">موجودی کیف پول</span>
+        <span class="input-group-text">تومان</span>
+        <input type="number" class="form-control" aria-label="Sizing example input"
+            aria-describedby="inputGroup-sizing-default" id="wallet" name="wallet">
+    </div>
+
+
+    <div>
+        <button class="btn btn-success" type="submit">ذخیره اطلاعات</button>
+    </div>
+
+
 
 </form>
