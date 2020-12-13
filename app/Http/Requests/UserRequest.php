855aff1339b096d364233c145cf9530ade916f23
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-12-13 11:25:55 +0330

    if not have message method in userRequest Messages read from lang Folder

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -32,16 +32,16 @@ class UserRequest extends FormRequest
     }
 
 
-    public function messages()
-    {
-        return [
-            'name.required' => 'وارد کردن نام الزامی است',
-            'email.required' => 'وارد کردن ایمیل الزامی است',
-            'email.email' => 'حتما ایمیل وارد کنید',
-            'password.required' => 'وارد کردن رمز عبور الزامی است',
-            'password.min' => 'کمترین مقدار 6 می باشد',
-            'password.max' => 'بیشتربن مقدار 12 می باشد',
-        ];
+    // public function messages()
+    // {
+    //     return [
+    //         'name.required' => 'وارد کردن نام الزامی است',
+    //         'email.required' => 'وارد کردن ایمیل الزامی است',
+    //         'email.email' => 'حتما ایمیل وارد کنید',
+    //         'password.required' => 'وارد کردن رمز عبور الزامی است',
+    //         'password.min' => 'کمترین مقدار 6 می باشد',
+    //         'password.max' => 'بیشتربن مقدار 12 می باشد',
+    //     ];
 
-    }
+    // }
 }
