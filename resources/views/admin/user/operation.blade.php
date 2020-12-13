385eebdba011dbe0bcec2684e4cbd110248eab40
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-12-13 13:55:19 +0330

    update operation route and font size

--- resources/views/admin/user/operation.blade.php
+++ resources/views/admin/user/operation.blade.php
@@ -1,3 +1,3 @@
-<a href=""><i class="far fa-edit" style="color:rgb(4, 0, 255)"></i></a>
-<a href="{{route('admin.user.delete',$item->id) }}"><i class="far fa-trash-alt" style="color:red"></i></a>
-<a href=""><i class="far fa-eye" style="color:green"></i></a>
+<a href="{{route('admin.user.edit',$item->id) }}"><i class="far fa-edit" style="color:rgb(4, 0, 255); font-size:24px;"></i></a>
+<a href="{{route('admin.user.delete',$item->id) }}"><i class="far fa-trash-alt" style="color:red; font-size:24px;"></i></a>
+<a href=""><i class="far fa-eye" style="color:green; font-size:24px;"></i></a>
