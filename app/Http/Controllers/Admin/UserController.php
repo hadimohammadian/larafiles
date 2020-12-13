0c988240a5abffc95fc036130192cb69e16e8476
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-12-13 12:32:55 +0330

    after insert data redirect to index page

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -59,7 +59,9 @@ class usercontroller extends Controller
 
         $new_user_object =  User::create($user_data);
         // dd($new_user_object);
-
+        if($new_user_object instanceof User){
+            return redirect()->route('admin.users.list');
+        }
 
 
     }
