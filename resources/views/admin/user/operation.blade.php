04fc3b9a62baf4038bf79f4b2e6d2c6614a63a78
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-12-13 13:45:59 +0330

    route to delete method and pass user_id to it

--- resources/views/admin/user/operation.blade.php
+++ resources/views/admin/user/operation.blade.php
@@ -1,3 +1,3 @@
 <a href=""><i class="far fa-edit" style="color:rgb(4, 0, 255)"></i></a>
-<a href=""><i class="far fa-trash-alt" style="color:red"></i></a>
+<a href="{{route('admin.user.delete',$item->id) }}"><i class="far fa-trash-alt" style="color:red"></i></a>
 <a href=""><i class="far fa-eye" style="color:green"></i></a>
