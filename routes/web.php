10fba9f39805ce7c7850132d43ccec1471bb359f
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-12-22 10:58:06 +0330

    add file route list

--- routes/web.php
+++ routes/web.php
@@ -27,10 +27,16 @@ Route::get('/admin', function () {
 
 
 Route::group(['prefix' => 'admin'], function () {
+
+    //Users Route
         Route::get('/user','App\Http\Controllers\Admin\UserController@index')->name('admin.users.list');
         Route::get('/user/create','App\Http\Controllers\Admin\UserController@create')->name('admin.users.create');
         Route::post('/user/create','App\Http\Controllers\Admin\UserController@store')->name('admin.users.store');
         Route::get('/users/delete/{user_id}','App\Http\Controllers\Admin\usercontroller@delete')->name('admin.user.delete');
         Route::get('/users/edit/{user_id}','App\Http\Controllers\Admin\usercontroller@edit')->name('admin.user.edit');
         Route::post('/users/edit/{user_id}','App\Http\Controllers\Admin\usercontroller@update')->name('admin.user.update');
+
+        //Files Routre
+
+        Route::get('/files', 'App\Http\Controllers\admin\FilesController@index')->name('admin.files.list');
 });
