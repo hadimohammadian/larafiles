eec234a8ffc92c0040ffc4bc608a15e1bf027447
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-12-22 11:42:54 +0330

    pass data from file Model

--- app/Http/Controllers/Admin/FilesController.php
+++ app/Http/Controllers/Admin/FilesController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\File;
 use Illuminate\Http\Request;
 
 class FilesController extends Controller
 {
     public function index(){
 
-        $files = null;
+        $files = File::all();
         return view('admin.File.list',compact('files'))->with(['panel_title' => 'لیست فایل ها']);
     }
 }
