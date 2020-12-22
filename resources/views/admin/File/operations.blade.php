2d9a51965830b571c04f14225015821090c63d61
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-12-22 12:15:34 +0330

    update operation link

--- resources/views/admin/File/operations.blade.php
+++ resources/views/admin/File/operations.blade.php
@@ -1,3 +1,3 @@
-<a href="#"><i class="far fa-edit" style="color:rgb(4, 0, 255); font-size:24px;"></i></a>
+<a href="{{ route('admin.files.edit',$item->file_id) }}"><i class="far fa-edit" style="color:rgb(4, 0, 255); font-size:24px;"></i></a>
 <a href="#"><i class="far fa-trash-alt" style="color:red; font-size:24px;"></i></a>
 <a href="#"><i class="far fa-eye" style="color:green; font-size:24px;"></i></a>
