5ddff8e0e99fb872325a81db567ff1a2905cc913
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-12-23 14:14:10 +0330

    store file in custome directory and fileName

--- app/Http/Controllers/Admin/FilesController.php
+++ app/Http/Controllers/Admin/FilesController.php
@@ -4,27 +4,32 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\File;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+use DateTime;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Date;
+use Symfony\Component\VarDumper\Cloner\Data;
 
 class FilesController extends Controller
 {
-    public function index(){
-
+    public function index()
+    {
         $files = File::all();
-        return view('admin.File.list',compact('files'))->with(['panel_title' => 'لیست فایل ها']);
+        return view('admin.File.list', compact('files'))->with(['panel_title' => 'لیست فایل ها']);
     }
 
-    public function create(){
-
-        return view('admin.File.create')->with('panel_title','ایجاد فایل جدید ');
+    public function create()
+    {
+        return view('admin.File.create')->with('panel_title', 'ایجاد فایل جدید ');
     }
 
-    public function store(Request $request){
-
+    public function store(Request $request)
+    {
         $this->validate($request, [
             'file_title' => 'required',
             'fileItem' => 'required'
-        ],[
+        ], [
             'file_title.required' => 'وارد کردن نام فایل الزامی است',
             'fileItem.required' => 'وارد کردن فایل الزامی است',
         ]);
@@ -37,10 +42,18 @@ class FilesController extends Controller
                 'file_size' => $request->file('fileItem')->getSize(),
 
             ];
-            $request->file('fileItem')->store('files/image');
+        // $request->file('fileItem')->store('files/image');
 
-            dd($new_file_data);
+        $file_extention = $request->file('fileItem')->getClientOriginalExtension();
+       // $time = Carbon::now()->format('Y/m/d-H:i:s') ;
+       $time = '';
 
+
+        $file_hash_name = Str::random(40);
+        $new_file_name = $file_hash_name.'-'. $time  .'.'. $file_extention;
+
+
+        $request->file('fileItem')->storeAs('images', $new_file_name);
 
 
     }
