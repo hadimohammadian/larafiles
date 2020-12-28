bd3b07b508302bcc422ed5a0e931ec0c66ea3ef6
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-12-28 12:00:33 +0330

    list and create paln method in plan controller

--- app/Http/Controllers/Admin/PlansController.php
+++ app/Http/Controllers/Admin/PlansController.php
@@ -7,5 +7,29 @@ use Illuminate\Http\Request;
 
 class PlansController extends Controller
 {
-    //
+    public function index(){
+        $plans  = '';
+return view('admin.plan.list',compact('plans'))->with(['panel_title' => 'لیست طرح ها']);
+
+
+
+}
+
+
+public function create(){
+    return view('admin.plan.create')->with(['panel_title' => 'ایجاد طرح جدید']);
+
+}
+
+public function store(Request $request){
+    $this->validate($request,[
+        'plan_title'=>'required',
+        'plan_limit_download_count' =>'required',
+        'plan_price' =>'required',
+        'plan_days_count'=>'required',
+
+    ]);
+}
+
+
 }
