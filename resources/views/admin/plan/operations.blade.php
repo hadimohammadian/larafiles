8040503aaf2fbbc597c12dedcd3058b04fec3001
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-12-28 12:03:17 +0330

    plans list operations

---
+++ resources/views/admin/plan/operations.blade.php
@@ -0,0 +1,3 @@
+<a href="{{ route('admin.plan.edit',$item->plan_id) }}"><i class="far fa-edit" style="color:rgb(4, 0, 255); font-size:24px;"></i></a>
+<a href="#"><i class="far fa-trash-alt" style="color:red; font-size:24px;"></i></a>
+<a href="#"><i class="far fa-eye" style="color:green; font-size:24px;"></i></a>
