cd92a07de6931deb61ffca8e01f4f9ab4a751f88
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-12-28 12:34:57 +0330

    add plans model

---
+++ app/Models/Plan.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Plan extends Model
+{
+    protected $primaryKey = 'plan_id';
+
+    protected $guarded = ['plan_id'];
+
+    public $timestamps = false;
+
+
+}
