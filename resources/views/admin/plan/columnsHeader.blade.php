e8d0f5b3a120d067cf7f9f5faa15b3634abc19bb
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-12-28 12:01:48 +0330

    plans list header

---
+++ resources/views/admin/plan/columnsHeader.blade.php
@@ -0,0 +1,8 @@
+<tr>
+
+    <th scope="col">عنوان</th>
+    <th scope="col">محدودیت دانلود</th>
+    <th scope="col">قیمت</th>
+    <th scope="col">تعداد روز اعتبار</th>
+    <th scope="col">عملیات</th>
+</tr>
